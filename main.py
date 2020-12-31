06034263578fe85250602111271fbede098d309f
Giovanni Lauria <giovanni.lauria@gmail.com>
2020-12-31 09:42:49 +0100

    File: main.py variable definitions

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import sys
 import os
+import platform
+import internal_functions
+
+if platform.system() == "Windows":
+    onWindows=True
+else:
+    onWindows=False
+
+numberOfArguments       = len(sys.argv)
+if (numberOfArguments==1):
+    internal_functions.Error
+elif(numberOfArguments==2):
+    detailedChanges         = sys.argv[1]
+elif (numberOfArguments==4): 
+    detailedChanges         = sys.argv[1]
+    switch                  = sys.argv[2]
+    release                 = sys.argv[3]
+else:
+    print ("Error !!!") 
+
+remoteAlias             = "origin"
+main_branch             = "main"
+
